ae334c605c6cda63b27e665ed729d2ce4cc079b3
Helge Sverre <helge.sverre@gmail.com>
2025-10-26 17:55:50 +0100

    feat(formatter): export formatter classes in main library
    Export BladeFormatter and FormatterConfig to make them available
    to users of the blade_parser package.

--- lib/blade_parser.dart
+++ lib/blade_parser.dart
@@ -8,6 +8,8 @@ export 'src/ast/visitor.dart';
 export 'src/ast/json_serializer.dart';
 export 'src/error/parse_error.dart';
 export 'src/error/parse_result.dart';
+export 'src/formatter/formatter.dart';
+export 'src/formatter/formatter_config.dart';
 export 'src/lexer/lexer.dart';
 export 'src/lexer/position.dart';
 export 'src/lexer/token.dart';
